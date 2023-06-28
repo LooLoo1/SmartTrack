530ef7e0e2f82df8fdb39f85de089407664f4619
Vitaliy Petriv <vamnir132132@gmail.com>
2023-06-28 13:03:24 +0300

    env update

--- frontend/src/apollo-client/client.ts
+++ frontend/src/apollo-client/client.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-	uri: `${import.meta.env.VITE_Server_Url}/graphql`,
+	uri: `${import.meta.env.VITE_SERVER_URL}/graphql`,
 	cache: new InMemoryCache(),
 });
